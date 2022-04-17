f9ba41c18f7efd949b09c313b9be7319058060c2
yyogesh <yogeshggn1@gmail.com>
2022-04-17 10:22:34 +0530

    header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Next Netflix App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      {/* Header */}
+      <Header />
+      <main >
+        {/* Banner */}
+        <section>
+          {/* Row */}
+          {/* Row */}
+        </section>
+      </main>
+      {/* modal */}
     </div>
   )
 }
