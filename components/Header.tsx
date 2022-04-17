006a9d83ef1a41486868b9d932e4484317085d75
yyogesh <yogeshggn1@gmail.com>
2022-04-17 10:37:13 +0530

    Header style

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+import Link from 'next/link';
 
 const Header = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true)
+            } else {
+                setIsScrolled(false)
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
     return (
-        <header>
+        <header className={`${isScrolled && 'bg-[#141414]'}`}>
             <div className='flex items-center space-x-2 md:space-x-10'>
                 <img
                     src="https://rb.gy/ulxxee"
@@ -18,8 +37,17 @@ const Header = () => {
                     <li className='headerLink'>My List</li>
                 </ul>
             </div>
-            <div>
-
+            <div className='flex items-center space-x-4 text-sm font-light'>
+                <SearchIcon className='hidden w-6 h-6 sm:inline' />
+                <p className="hidden lg:inline">Kids</p>
+                <BellIcon className="w-6 h-6" />
+                <Link href='/account'>
+                    <img
+                        src="https://rb.gy/g1pwyx"
+                        alt=""
+                        className="rounded cursor-pointer"
+                    />
+                </Link>
             </div>
         </header>
     )
